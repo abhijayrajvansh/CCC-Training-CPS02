69fdb57481e56ee199c5e958a81944f4adcf1083
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-09-16 10:33:32 +0530

    Adding Dynamic Allocation

---
+++ dynamic-memory-allocation.cpp
@@ -0,0 +1,18 @@
+/**
+ *    author:   abhijayrajvansh
+ *    created:  16.09.2022 10:11:41
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Sample{
+    int a, b;
+};
+
+int main (){
+    Sample re, *pe;
+    re.a = 88, re.b = 99;
+    cout << re.a << " " << re.b << '\n';
+    cout << pe -> a, pe -> b;  
+    return 0;    
+}
